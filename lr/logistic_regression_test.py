a865729c7a67ce250ea7938b35c837c3b79dc37b
liangbin <liangbinhlw@bbktel.com>
2019-09-11 16:24:20 +0800

    pic

--- lr/logistic_regression_test.py
+++ lr/logistic_regression_test.py
@@ -7,6 +7,19 @@ import matplotlib as mpl
 mpl.rcParams['font.sans-serif'] = [u'SimHei']
 mpl.rcParams['axes.unicode_minus'] = False
 
+def show_pic(X,y,line):
+    x1 = np.linspace(0, 10, 1000)
+    x2 = np.linspace(0, 10, 1000)
+    x1, x2 = np.meshgrid(x1, x2)
+    x_show = np.stack((x1.flat, x2.flat), axis=1)
+    y_pret = line.predict(x_show)
+    cm_light = mpl.colors.ListedColormap(['#A0FFA0', '#FFA0A0', '#A0A0FF'])
+    cm_dark = mpl.colors.ListedColormap(['g', 'r', 'b'])
+    plt.pcolormesh(x1, x2, y_pret.reshape(x1.shape), cmap=cm_light)
+    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=cm_dark)
+    plt.show()
+
+
 if __name__ == '__main__':
     # 样本
     X = np.array([[1, 2], [2, 4], [5, 3.02], [10, 5.01]])
@@ -18,47 +31,9 @@ if __name__ == '__main__':
     # 轮子
     liner = HasikaLogisticRegression()
     liner.fit(X, y)
+    show_pic(X, y, liner)
 
     # scikit-learn 类库
     liner2 = LogisticRegression()
     liner2.fit(X, y)
-
-    # 画轮子
-    x1 = np.linspace(0, 10, 1000)
-    x2 = np.linspace(0, 10, 1000)
-    x1, x2 = np.meshgrid(x1, x2)
-    x_show = np.stack((x1.flat, x2.flat), axis=1)
-    y_pret = liner.predict(x_show)
-    print('轮子 回归系数, 截距')
-    print(liner.coef_, liner.intercept_)
-
-    cm_light = mpl.colors.ListedColormap(['#A0FFA0', '#FFA0A0', '#A0A0FF'])
-    cm_dark = mpl.colors.ListedColormap(['g', 'r', 'b'])
-
-    print("轮子：", y_pret.shape)
-    plt.pcolormesh(x1, x2, y_pret.reshape(x1.shape), cmap=cm_light)
-    print("轮子：", x1.shape)
-    print("轮子：", y_pret.shape)
-    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=cm_dark)
-
-    plt.show()
-
-    # 画scikit-learn类库函数线
-    x1 = np.linspace(0, 10, 1000)
-    x2 = np.linspace(0, 10, 1000)
-    x1, x2 = np.meshgrid(x1, x2)
-    x_show = np.stack((x1.flat, x2.flat), axis=1)
-    y_pret = liner2.predict(x_show)
-    print('scikit-learn类库 回归系数, 截距')
-    print(liner2.coef_, liner2.intercept_)
-    # b2 = liner2.intercept_ + a * liner2.coef_
-
-    cm_light = mpl.colors.ListedColormap(['#A0FFA0', '#FFA0A0', '#A0A0FF'])
-    cm_dark = mpl.colors.ListedColormap(['g', 'r', 'b'])
-
-    print("类库：", y_pret.shape)
-    plt.pcolormesh(x1, x2, y_pret.reshape(x1.shape), cmap=cm_light)
-    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=cm_dark)
-    print("类库：", x1.shape)
-    print("类库：", y_pret.shape)
-    plt.show()
+    show_pic(X, y, liner2)
